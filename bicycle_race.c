de99c07a8b164f96c102203c8c44fcbd4396dadc
Lucas Alexander Rodrigues Lima <88013886+LuceNori@users.noreply.github.com>
2021-08-04 15:50:05 -0400

    Add files via upload

---
+++ bicycle_race.c
@@ -0,0 +1,10 @@
+//D. Bicycle Race
+
+#include <stdio.h>
+int a;	
+	int main(){
+
+	scanf("%d", &a);
+	printf("%d\n",(a-4)/2);
+return 0;
+}
---
+++ lucky.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+long long n=0;
+int a=0;
+int main(){
+	scanf("%lld",&n);
+	while(n!=0){
+		if(n%10==7||n%10==4)
+			a++;
+			n=n/10;
+	}
+	if(a==7||a==4){
+		printf("YES");
+	}else{
+		printf("NO");
+	}
+	return 0;
+}
